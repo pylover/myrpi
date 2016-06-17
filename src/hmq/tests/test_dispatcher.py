5328d501512f67f9f411240182ce1214978c7dd7
vahid <vahid.mardani@gmail.com>
2016-06-17 20:59:31 +0430

    async test

--- src/hmq/tests/test_dispatcher.py
+++ src/hmq/tests/test_dispatcher.py
@@ -1,2 +1,15 @@
-# -*- coding: utf-8 -*-
-__author__ = 'vahid'
+import unittest
+from hmq.tests.aio import AioTestCase
+
+
+class TestDispatcher(AioTestCase):
+
+    def test_easy(self):
+        self.assertEqual(1, 1)
+
+    async def test_complex(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
