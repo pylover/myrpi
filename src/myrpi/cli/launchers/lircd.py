8fc3039c05bdb3474221ea27b132c83d0163dca1
vahid <vahid@dobisel.com>
2016-09-25 14:10:04 +0330

    Handling asyncio exceptions

--- src/myrpi/cli/launchers/lircd.py
+++ src/myrpi/cli/launchers/lircd.py
@@ -17,12 +17,12 @@ class LIRCdLauncher(ConfiguredLauncher):
     async def launch(self):
         from myrpi.car_audio import RPIGPIOContext
 
-        async with LIRCClient(
+        async with RPIGPIOContext(), LIRCClient(
                 settings.lirc.lircrc_prog, lircrc_file=settings.lirc.lircrc_file,
-                check_interval=settings.lirc.check_interval) as client, \
-                RPIGPIOContext():
+                check_interval=settings.lirc.check_interval) as client:
             dispatcher = IRCDispatcher(client)
             result = (await asyncio.gather(dispatcher.listen(), return_exceptions=True))[0]
+            print('###############', result)
 
         if isinstance(result, Exception):
             raise result
